7db0cf151ca0c3fa2416766e7d788597cbd91bf4
Phil <filip.nyman7@gmail.com>
2023-08-29 08:31:06 +0200

    created to use for prompts in terminal

---
+++ prompts.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")();
+
+const elevtorCall = prompt(
+  "Call elevator by typing Call followed by the desired floor number."
+);
+
+const elevatorStatus = prompt("Get Elevator Status by typing Status");
