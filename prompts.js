28550a92950c15d6d061e48ab79834c4450a1e12
Phil <filip.nyman7@gmail.com>
2023-08-31 15:33:52 +0200

    a prompt func taking prompt to call elevator

--- prompts.js
+++ prompts.js
@@ -1,7 +1,31 @@
 const prompt = require("prompt-sync")();
 
-const elevtorCall = prompt(
-  "Call elevator by typing Call followed by the desired floor number."
-);
+//Prompt to Call elevator to to x
+//1.prompta användare att kunna göra calls
+function promptElevator() {
+  const elevatorCall = prompt(
+    "Call elevator by typing: Call to (desired floor)"
+  );
 
+  //2.ta user prompt
+  const desiredFloor = parseInt(elevatorCall.split(" ").pop());
+
+  //3.parsa input till metoden
+
+  //4.kontrollera att våningen finns inom 1-10
+  if (desiredFloor < 0 && desiredFloor > 10) {
+    console.log("Error; Choose a floor between 1-10");
+    promptElevator();
+
+    //5.om våningen är giltig:kalla metoden till rättvåning
+    //b. visa meddelande att hissen förflyttats
+  } else {
+    this.callElevator(desiredFloor);
+  }
+}
+promptElevator();
+
+//7.upprepa steg ovan till rätt prompt tagits
+
+//Prompt to get elevatorStatus:
 const elevatorStatus = prompt("Get Elevator Status by typing Status");
