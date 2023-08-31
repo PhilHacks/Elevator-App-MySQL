e4e56946cc171deb0d1a58459d5e71bfc3a9b277
Phil <filip.nyman7@gmail.com>
2023-08-31 15:50:54 +0200

    started pseudocode for elevatorStatusPrompt

--- prompts.js
+++ prompts.js
@@ -7,14 +7,13 @@ function promptElevator() {
     "Call elevator by typing: Call to (desired floor)"
   );
 
-  //2.ta user prompt
+  //2.parsa input
   const desiredFloor = parseInt(elevatorCall.split(" ").pop());
 
-  //3.parsa input till metoden
-
-  //4.kontrollera att våningen finns inom 1-10
+  //3.kontrollera att våningen finns inom 1-10
   if (desiredFloor < 0 && desiredFloor > 10) {
     console.log("Error; Choose a floor between 1-10");
+    //4. Kör igen om våningen inte finns
     promptElevator();
 
     //5.om våningen är giltig:kalla metoden till rättvåning
@@ -25,7 +24,14 @@ function promptElevator() {
 }
 promptElevator();
 
-//7.upprepa steg ovan till rätt prompt tagits
-
 //Prompt to get elevatorStatus:
-const elevatorStatus = prompt("Get Elevator Status by typing Status");
+function elevatorStatusPrompt() {
+  const elevatorStatus = prompt("Get Elevator Status by typing Status");
+  this.displayElevatorStatus(elevatorStatus);
+
+  //1. Implement so user can type "Elevator Status" in terminal
+  //2. call on displayElevatorStatus
+  //3. return all current floors on all elevators
+}
+
+elevatorStatusPrompt();
