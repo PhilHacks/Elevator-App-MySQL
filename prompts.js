498db82f6004927f6f2a81474e4b9ac285bb89f9
Phil <filip.nyman7@gmail.com>
2023-09-05 17:00:36 +0200

    Gjorde klart prompts.js

--- prompts.js
+++ prompts.js
@@ -1,37 +1,44 @@
 const prompt = require("prompt-sync")();
 
 //Prompt to Call elevator to to x
-//1.prompta användare att kunna göra calls
 function promptElevator() {
   const elevatorCall = prompt(
     "Call elevator by typing: Call to (desired floor)"
   );
-
-  //2.parsa input
+  //2. parse input from userinput
   const desiredFloor = parseInt(elevatorCall.split(" ").pop());
 
   //3.kontrollera att våningen finns inom 1-10
-  if (desiredFloor < 0 && desiredFloor > 10) {
-    console.log("Error; Choose a floor between 1-10");
-    //4. Kör igen om våningen inte finns
-    promptElevator();
-
-    //5.om våningen är giltig:kalla metoden till rättvåning
-    //b. visa meddelande att hissen förflyttats
+  if (desiredFloor >= 1 && desiredFloor <= 10) {
+    elevatorSystem.callElevator(desiredFloor);
   } else {
-    this.callElevator(desiredFloor);
+    console.log("Error: Choose a floor between 1-10");
+    //4. Recursive call om våningen inte finns
+    promptElevator();
   }
 }
 promptElevator();
 
 //Prompt to get elevatorStatus:
 function elevatorStatusPrompt() {
-  const elevatorStatus = prompt("Get Elevator Status by typing Status");
-  this.displayElevatorStatus(elevatorStatus);
+  const elevatorStatusInput = prompt(
+    "Get Elevator Status by typing 'Status'"
+  ).toLowerCase();
 
-  //1. Implement so user can type "Elevator Status" in terminal
-  //2. call on displayElevatorStatus
-  //3. return all current floors on all elevators
+  if (elevatorStatusInput === "status") {
+    const elevatorStatusList = displayElevatorStatus.call(this);
+
+    // Log the elevator status
+    console.log("Elevator Status:");
+    for (const elevatorStatus of elevatorStatusList) {
+      console.log(
+        `Elevator ${elevatorStatus.elevatorId}: Current Floor: ${elevatorStatus.currentFloor}, isMoving: ${elevatorStatus.isMoving}, currentStatus: ${elevatorStatus.currentStatus}`
+      );
+    }
+  } else {
+    console.log("Invalid command. Please type 'Status' ");
+    // You can handle invalid commands as needed, e.g., repeat the prompt
+    elevatorStatusPrompt();
+  }
 }
-
 elevatorStatusPrompt();
