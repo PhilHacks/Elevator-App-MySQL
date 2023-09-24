6d5ffae34e49a5ae60767d0a54221a93c6606dcc
Phil <filip.nyman7@gmail.com>
2023-09-24 21:31:11 +0200

    latest version

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const ElevatorSystem = require("./elevatorSystem");
+
+const elevatorSystem = new ElevatorSystem();
+const app = express();
+
+app.use(express.json()); // Middleware för att kunna läsa JSON-data från POST requests.
+
+// Route för att kalla på hiss
+app.post("/callElevator", async (req, res) => {
+  const floor = req.body.floor;
+
+  if (typeof floor === "undefined" || floor === null) {
+    res.status(400).send("Bad Request: floor parameter missing.");
+    return;
+  }
+  await elevatorSystem.callElevator(floor);
+  res.send(`Calling elevator to floor ${floor}`);
+});
+
+// Route för att hämta statusen för alla hissar
+app.get("/elevator/status", (req, res) => {
+  const elevatorStatus = elevatorSystem.getElevatorStatus();
+  res.json(elevatorStatus);
+});
+
+// Route för att kontrollera om en specifik hiss är tillgänglig
+app.get("/elevator/available/:elevatorId", (req, res) => {
+  const elevatorId = parseInt(req.params.elevatorId);
+
+  if (isNaN(elevatorId)) {
+    res.status(400).send("Invalid elevatorId.");
+    return;
+  }
+
+  const isAvailable = elevatorSystem.isElevatorAvailable(elevatorId);
+
+  if (isAvailable) {
+    res.send(`Elevator with ID ${elevatorId} is available.`);
+  } else {
+    res.send(`Elevator with ID ${elevatorId} is not available.`);
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+module.exports = { app, elevatorSystem };
--- elevator.js
+++ elevator.js
@@ -1,50 +1,51 @@
-//1.Elevator Class(blueprint) with properties(attributes)
-export default class Elevator {
+//Elevator Class(blueprint) with properties(attributes)
+class Elevator {
   constructor(elevatorId) {
     this.elevatorId = elevatorId;
     this.currentFloor = 1;
     this.currentStatus = "idle"; //  lagrar hissens status idle, movingUp, movingDown
     this.isMoving = false;
     this.floorTravelTime = 2000; // 2sec
-    this.numFloors = 10;
     this.statusHistory = [];
   }
 
-  //1.1 Method() to move Elevator
+  //Method() to move Elevator
   goToFloor(destinationFloor) {
-    // Calculate travel time
-    const travelTime =
-      Math.abs(destinationFloor - this.currentFloor) * this.floorTravelTime;
+    return new Promise((resolve, reject) => {
+      // Calculate travel time
+      const travelTime =
+        Math.abs(destinationFloor - this.currentFloor) * this.floorTravelTime;
 
-    // Set the elevator to moving state
-    if (destinationFloor > this.currentFloor) {
-      this.currentStatus = "movingUp";
-    } else if (destinationFloor < this.currentFloor) {
-      this.currentStatus = "movingDown";
-    }
-    this.isMoving = true;
-    console.log(
-      "Moving from floor",
-      this.currentFloor,
-      "to floor",
-      destinationFloor
-    );
+      // Set elevator to moving state
+      if (destinationFloor > this.currentFloor) {
+        this.currentStatus = "movingUp";
+      } else if (destinationFloor < this.currentFloor) {
+        this.currentStatus = "movingDown";
+      }
+      this.isMoving = true;
+      console.log(
+        "Moving from floor",
+        this.currentFloor,
+        "to floor",
+        destinationFloor
+      );
 
-    //1.2 Use setTimeout to simulate travel time and update the current floor
-    setTimeout(() => {
-      this.currentFloor = destinationFloor;
-      this.isMoving = false;
-      console.log("Arrived at floor", this.currentFloor);
+      //SetTimeout simulate travel time
+      setTimeout(() => {
+        this.currentFloor = destinationFloor;
+        this.isMoving = false;
+        console.log("Arrived at floor", this.currentFloor);
 
-      // Update the status array
-      this.statusHistory.push({
-        floor: this.currentFloor,
-        isMoving: this.isMoving,
-        currentStatus: this.currentStatus,
-      });
-    }, travelTime);
+        // Update the status array
+        this.statusHistory.push({
+          floor: this.currentFloor,
+          isMoving: this.isMoving,
+          currentStatus: this.currentStatus,
+        });
+        resolve();
+      }, travelTime);
+    });
   }
 }
 
-//Exportera
 module.exports = Elevator;
