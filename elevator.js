132750e4ba3ff4af45024e954478a105d136dc35
Phil <filip.nyman7@gmail.com>
2023-11-09 10:03:51 +0100

    createTravelTime method to improve reusability

--- elevator.js
+++ elevator.js
@@ -9,46 +9,56 @@ class Elevator {
     this.statusHistory = [];
   }
 
-  //Method() to move Elevator
   goToFloor(destinationFloor) {
     return new Promise((resolve, reject) => {
-      // Calculate travel time
-      const travelTime =
-        Math.abs(destinationFloor - this.currentFloor) * this.floorTravelTime;
-
-      // Set elevator to moving state
-      if (destinationFloor > this.currentFloor) {
-        this.currentStatus = "moving_Up";
-      } else if (destinationFloor < this.currentFloor) {
-        this.currentStatus = "moving_Down";
-      }
-      this.isMoving = true;
-      console.log(
-        `Elevator ${this.elevatorId} is moving from floor`,
-        this.currentFloor,
-        "to floor",
-        destinationFloor
-      );
-
-      //SetTimeout simulate travel time
-      setTimeout(() => {
-        this.currentFloor = destinationFloor;
-        this.isMoving = false;
-        this.currentStatus = "idle";
-        console.log(
-          `Elevator ${this.elevatorId} arrived at floor ${this.currentFloor}`
-        );
-
-        // Update the status array
-        this.statusHistory.push({
-          floor: this.currentFloor,
-          isMoving: this.isMoving,
-          currentStatus: this.currentStatus,
-        });
-        resolve();
-      }, travelTime);
+      const travelTime = this.calculateTravelTime(destinationFloor);
     });
   }
-}
+  
+  calculateTravelTime (destinationFloor) {
+    return Math.abs(destinationFloor - this.currentFloor) * this.floorTravelTime;
+  }
+     
+
+//   setToMovingState (destinationFloor) {
+//     // Set elevator to moving state
+//     if (destinationFloor > this.currentFloor) {
+//       this.currentStatus = "moving_Up";
+//     } else if (destinationFloor < this.currentFloor) {
+//       this.currentStatus = "moving_Down";
+//     }
+//     this.isMoving = true;
+//     console.log(
+//       `Elevator ${this.elevatorId} is moving from floor`,
+//       this.currentFloor,
+//       "to floor",
+//       destinationFloor
+//     );
+//   }
+// }  
+
+//   simulateTravelTime () {
+//     //SetTimeout simulateTravelTime
+//     setTimeout(() => {
+//       this.currentFloor = destinationFloor;
+//       this.isMoving = false;
+//       this.currentStatus = "idle";
+//       console.log(
+//         `Elevator ${this.elevatorId} arrived at floor ${this.currentFloor}`
+//       );
+//   } 
+      
+
+//   updateStatusArr();      
+//   // Update the status array
+//         this.statusHistory.push({
+//           floor: this.currentFloor,
+//           isMoving: this.isMoving,
+//           currentStatus: this.currentStatus,
+//         });
+//         resolve();
+//       }, travelTime);
+//     });
+  
 
 module.exports = Elevator;
