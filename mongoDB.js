9859c2f9fb493e57b99c5bcfd7bc12cca2e83dd1
Phil <filip.nyman7@gmail.com>
2023-11-14 20:21:26 +0100

    change to capital letter in schema

--- mongoDB.js
+++ mongoDB.js
@@ -12,12 +12,12 @@ mongoose
 
 const elevatorSchema = new mongoose.Schema({
   id: String,
-  currentFloor: number,
+  currentFloor: Number,
   status: {
     type: String,
     enum: ["idle", "moving_up", "moving_down"],
   },
-  destinationFloor: number,
+  destinationFloor: Number,
 });
 
 export const ElevatorModel = mongoose.model("Elevator", elevatorSchema);
