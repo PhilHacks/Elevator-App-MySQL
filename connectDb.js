0322fce05ba4af79b2061b673d652ad464a85396
Phil <filip.nyman7@gmail.com>
2023-11-14 16:16:36 +0100

    create connection mongodb via .env file for safety

---
+++ connectDb.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { MONGODB_URI } = process.env;
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
