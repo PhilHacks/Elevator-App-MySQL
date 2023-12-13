643549de2a5a2647416a029fb2542a689b01b5e8
Phil <filip.nyman7@gmail.com>
2023-12-13 13:24:21 +0100

    moved func to crud cause it is related to db

--- elevatorManager.js
+++ elevatorManager.js
@@ -1,6 +1,10 @@
 // import Elevator from "./elevator.js";
 import { ElevatorModel } from "./elevatorModel.js";
-import { updateElevatorDB } from "./crudOperations.js";
+import {
+  updateElevatorDB,
+  findIdleElevators,
+  checkIfElevatorOnFloor,
+} from "./crudOperations.js";
 
 class ElevatorManager {
   constructor() {
@@ -9,13 +13,9 @@ class ElevatorManager {
     this.checkQueueInterval = setInterval(() => this.processQueue(), 500); // Check every 0.5 seconds
   }
 
-  async findIdleElevators() {
-    return await ElevatorModel.find({ currentStatus: "idle" });
-  }
-
   async findClosestElevator(destinationFloor) {
     try {
-      const idleElevators = await this.findIdleElevators();
+      const idleElevators = await findIdleElevators();
       const sortedElevators = this.sortIdleElevatorsByDistance(
         idleElevators,
         destinationFloor
@@ -41,9 +41,7 @@ class ElevatorManager {
   async handleElevatorCalls(destinationFloor) {
     try {
       this.checkInvalidFloorReq(destinationFloor);
-      const elevatorOnFloor = await this.checkIfElevatorOnFloor(
-        destinationFloor
-      );
+      const elevatorOnFloor = await checkIfElevatorOnFloor(destinationFloor);
 
       if (!elevatorOnFloor) {
         this.callOrQueueElevator(destinationFloor);
@@ -57,18 +55,6 @@ class ElevatorManager {
     if (destinationFloor < 1 || destinationFloor > this.numberOfFloors) {
       throw new Error("Invalid floor requested.");
     }
-  }
-
-  async checkIfElevatorOnFloor(destinationFloor) {
-    const elevatorOnFloor = await ElevatorModel.findOne({
-      currentFloor: destinationFloor,
-      currentStatus: "idle",
-    });
-    if (elevatorOnFloor) {
-      console.log(`Elevator already at floor ${destinationFloor}`);
-      return true;
-    }
-    return false;
   }
 
   async callOrQueueElevator(destinationFloor) {
