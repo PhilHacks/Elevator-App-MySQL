ac432efccba4d07399c97f41766da90bf7f6eb8c
Phil <filip.nyman7@gmail.com>
2023-12-29 21:56:30 +0100

    removed mongodb connection

--- dbConnect.js
+++ dbConnect.js
@@ -1,11 +1,1 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const { MONGODB_URI } = process.env;
-
-export const dbConnection = mongoose
-  .connect(MONGODB_URI)
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB...", err));
