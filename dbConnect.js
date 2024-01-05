cca99b0cd2536c59a6570f3211138270f167e636
Phil <filip.nyman7@gmail.com>
2024-01-05 09:00:23 +0100

    establish connect tested and works

--- dbConnect.js
+++ dbConnect.js
@@ -1,1 +1,24 @@
+import dotenv from "dotenv"; //.env for security
+dotenv.config();
 
+import mysql from "mysql2";
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "sql_elevators",
+});
+
+// Connect to the MySQL server
+connection.connect((error) => {
+  if (error) {
+    console.error("Error connecting: " + error.stack);
+    return;
+  }
+  console.log("Connected as id " + connection.threadId);
+});
+
+// Close the connection
+connection.end();
