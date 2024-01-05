b260c35b5595f5fbfe434ed9017b47a0b1972b78
Phil <filip.nyman7@gmail.com>
2024-01-05 10:02:20 +0100

    created db and table for db tested it works

---
+++ createdb.sql
@@ -0,0 +1,17 @@
+DROP DATABASE `sql_elevators`;
+
+CREATE DATABASE IF NOT EXISTS `sql_elevators`;
+USE `sql_elevators`;
+
+
+CREATE TABLE IF NOT EXISTS elevators (
+    elevator_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    current_Floor INT DEFAULT 0,
+    currentStatus ENUM('idle', 'moving_up', 'moving_down') DEFAULT 'idle',
+    destination_Floor INT DEFAULT NULL,
+    callQueue JSON 
+);
+
+INSERT INTO elevators (callQueue) VALUES ('[]');
+INSERT INTO elevators (callQueue) VALUES ('[]');
+INSERT INTO elevators (callQueue) VALUES ('[]');
