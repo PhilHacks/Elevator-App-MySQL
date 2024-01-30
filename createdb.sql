2925d15de55a3476fe2bf6088f3c108f26269bb8
Phil <filip.nyman7@gmail.com>
2024-01-30 16:48:08 +0100

    add table for call_queue

--- createdb.sql
+++ createdb.sql
@@ -6,12 +6,16 @@ USE `sql_elevators`;
 
 CREATE TABLE IF NOT EXISTS elevators (
     elevator_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    current_Floor INT DEFAULT 0,
-    current_Status ENUM('idle', 'moving_up', 'moving_down') DEFAULT 'idle',
-    destination_Floor INT DEFAULT NULL,
-    callQueue JSON 
+    current_floor INT DEFAULT 0,
+    current_status ENUM('idle', 'moving_up', 'moving_down') DEFAULT 'idle',
+    destination_floor INT DEFAULT NULL 
 );
 
-INSERT INTO elevators (callQueue) VALUES ('[]');
-INSERT INTO elevators (callQueue) VALUES ('[]');
-INSERT INTO elevators (callQueue) VALUES ('[]');
+INSERT INTO elevators (current_floor, current_status) VALUES (0, 'idle');
+INSERT INTO elevators (current_floor, current_status) VALUES (0, 'idle');
+INSERT INTO elevators (current_floor, current_status) VALUES (0, 'idle');
+
+ CREATE TABLE IF NOT EXISTS call_queue (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    destination_floor INT NOT NULL
+);
