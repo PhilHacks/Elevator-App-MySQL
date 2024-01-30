fc637e92089a1a51b82cfb4cf1c18b264b204b84
Phil <filip.nyman7@gmail.com>
2024-01-30 16:50:35 +0100

    new logic in main to startServer and connect to db

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 import express from "express";
-import { dbConnection } from "./dbConnect.js";
-import { checkIfElevatorDocumentExist } from "./elevatorModel.js";
+import pool from "./dbConnect.js";
 import routes from "./routes.js";
 
 export const app = express();
+const port = process.env.PORT || 3000;
 
-try {
-  const port = process.env.PORT || 3000;
+async function startServer() {
+  try {
+    // Attempt to get a database connection
+    const connection = await pool.getConnection();
+    console.log("Connected to the database");
+    connection.release();
 
-  //make sure connetion to db and that ElevatorDoc exists before server starts
-  dbConnection.then(() => {
-    checkIfElevatorDocumentExist();
+    // Start the server
     app.use(routes);
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
-  });
-} catch (error) {
-  console.error(
-    "An error occurred during server initialization:",
-    error.message
-  );
+  } catch (error) {
+    console.error("Error connecting to the database: " + error.message);
+    // Handle error or exit process
+    process.exit(1);
+  }
 }
+
+startServer();
