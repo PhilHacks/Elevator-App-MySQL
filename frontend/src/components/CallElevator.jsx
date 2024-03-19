3cb8d1163677294f370e4f67912769fcf0532dd8
Phil <filip.nyman7@gmail.com>
2024-03-19 10:09:27 +0100

    change from alert to setMessage

--- frontend/src/components/CallElevator.jsx
+++ frontend/src/components/CallElevator.jsx
@@ -3,22 +3,32 @@ import axios from 'axios'
 
 function CallElevator() {
 const [floor, setFloor] = useState("");
+const [message, setMessage] = useState ("") 
 
 
 
-const submit = () => {
-    axios.post("http://localhost:5000/callElevator", {floor: floor})
-    .then(response => alert("Elevator called"))
-    .catch(error => alert("Something went wrong"));
+const submitCall = () => {
+    axios.post("http://localhost:5000/callElevator", {floor: Number(floor) })
+      .then(response => {
+       setMessage(response.data.message);
+    })
+    .catch(error => {
+      setMessage(error.response?.data?.message || "Something went wrong");
+   })
+    .finally(() => {
+      setTimeout(() => {setMessage(""); setFloor("");}, 3000);
+    });
+    
 };
-
 
 
   return (
     <div>
     <h2>Call Elevator</h2>
-    <input type="number" value={floor} onChange={e => setFloor(e.target.value)}></input>
-    <button onClick={submit}>Call Elevator</button>
+    <input type="number" value={floor} onChange={e => setFloor(e.target.value)} placeholder="Enter floor number"></input>
+    
+    <button onClick={submitCall}>Call Elevator</button>
+    {message && <div>{message}</div>}
     </div>
   )
 }
