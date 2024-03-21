94caa708fcdd9c72d6a2c586f1033c7f473c3814
Phil <filip.nyman7@gmail.com>
2024-03-21 16:55:00 +0100

    added two import to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@ import "./App.css";
 import ElevatorStatus from "./components/ElevatorStatus";
 import CallElevator from "./components/CallElevator";
 import UpdateStatus from "./components/UpdateStatus";
+import CallQueue from "./components/CallQueue";
+// import { getElevators } from "./services/ElevatorService";
+
 function App() {
   return (
     <div>
       <ElevatorStatus></ElevatorStatus>
+      <CallQueue></CallQueue>
       <CallElevator></CallElevator>
       <UpdateStatus></UpdateStatus>
     </div>
