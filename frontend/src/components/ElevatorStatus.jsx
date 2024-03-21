a64bde210711e5c5bbdb837c869b93958ff7be10
Phil <filip.nyman7@gmail.com>
2024-03-21 16:57:00 +0100

    reformatted w prettier & changed li key

--- frontend/src/components/ElevatorStatus.jsx
+++ frontend/src/components/ElevatorStatus.jsx
@@ -1,45 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function ElevatorStatus() {
-const [status, setStatus] = useState([]);
-const [message, setMessage] =useState("");
+  const [status, setStatus] = useState([]);
+  const [message, setMessage] = useState("");
 
-useEffect(() => {
-  const fetchElevatorStatus = () => {
-    axios.get("http://localhost:5000/elevator/status")
-      .then(response => {
-        setStatus(response.data)
-        setMessage(""); 
-      })
-      .catch(error => {
-        const errorMessage = error.response?.data?.message || "Unable to fetch elevator status. Please try again later.";
-        setMessage(errorMessage)
-      });
-  };
+  useEffect(() => {
+    const fetchElevatorStatus = () => {
+      axios
+        .get("http://localhost:5000/elevator/status")
+        .then((response) => {
+          setStatus(response.data);
+          setMessage("");
+        })
+        .catch((error) => {
+          const errorMessage =
+            error.response?.data?.message ||
+            "Unable to fetch elevator status. Please try again later.";
+          setMessage(errorMessage);
+        });
+    };
 
- fetchElevatorStatus();
+    fetchElevatorStatus();
+    const statusInterval = setInterval(fetchElevatorStatus, 2000);
+    return () => clearInterval(statusInterval);
+  }, []);
 
-  const statusInterval = setInterval(fetchElevatorStatus, 2000);
-
-  return () => clearInterval(statusInterval);
-}, []);
-
-return (
+  return (
     <div>
       <h2>Elevator Status</h2>
       <ul>
-        {status.map(elevator => (
-          <li key={elevator.id}>
-            Elevator {elevator.elevatorId}: Currently at floor {elevator.currentFloor}, Status: {elevator.currentStatus}
+        {status.map((elevator) => (
+          <li key={elevator.elevatorId}>
+            Elevator {elevator.elevatorId}: Currently at floor{" "}
+            {elevator.currentFloor}, Status: {elevator.currentStatus}
           </li>
         ))}
       </ul>
       {message && <div>{message}</div>}
     </div>
   );
-};
+}
 
-export default ElevatorStatus
+export default ElevatorStatus;
