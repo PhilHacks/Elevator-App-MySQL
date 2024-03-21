1f88a3395c12b852944ba22947c2756b2d6c1620
Phil <filip.nyman7@gmail.com>
2024-03-21 16:55:52 +0100

    refactored with prettier

--- frontend/src/components/CallElevator.jsx
+++ frontend/src/components/CallElevator.jsx
@@ -1,36 +1,42 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
 function CallElevator() {
-const [floor, setFloor] = useState("");
-const [message, setMessage] = useState ("") 
+  const [floor, setFloor] = useState("");
+  const [message, setMessage] = useState("");
 
-
-
-const submitCall = () => {
-    axios.post("http://localhost:5000/callElevator", {floor: Number(floor) })
-      .then(response => {
-       setMessage(response.data.message);
-    })
-    .catch(error => {
-      setMessage(error.response?.data?.message || "Something went wrong");
-   })
-    .finally(() => {
-      setTimeout(() => {setMessage(""); setFloor("");}, 3000);
-    });
-    
-};
-
+  const submitCall = () => {
+    axios
+      .post("http://localhost:5000/callElevator", { floor: Number(floor) })
+      .then((response) => {
+        setMessage(response.data.message);
+        setFloor("");
+      })
+      .catch((error) => {
+        setMessage(error.response?.data?.message || "Something went wrong");
+        setFloor("");
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setMessage("");
+        }, 3000);
+      });
+  };
 
   return (
     <div>
-    <h2>Call Elevator</h2>
-    <input type="number" value={floor} onChange={e => setFloor(e.target.value)} placeholder="Enter floor number"></input>
-    
-    <button onClick={submitCall}>Call Elevator</button>
-    {message && <div>{message}</div>}
+      <h2>Call Elevator</h2>
+      <input
+        type="number"
+        value={floor}
+        onChange={(e) => setFloor(e.target.value)}
+        placeholder="Enter floor number"
+      ></input>
+
+      <button onClick={submitCall}>Call Elevator</button>
+      {message && <div>{message}</div>}
     </div>
-  )
+  );
 }
 
-export default CallElevator
+export default CallElevator;
